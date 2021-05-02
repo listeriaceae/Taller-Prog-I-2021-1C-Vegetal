ef6a99b59494cc304f38a57a47ed8a4785a72aa8
cdrubio <crubio@fi.uba.ar>
2021-05-02 09:16:32 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
-#include <fstream>
-
-// #include "configuration.hpp"
-
-#include <jsoncpp/json/json.h>
-#include <jsoncpp/json/value.h>
+#include "configuration.hpp"
 
 int main(void)
 {
-    Json::Value cfg_root;
-    std::ifstream cfgfile("default_configuration.json");
-    cfgfile >> cfg_root;
+    auto configuration = configuration::Configuration("asd.json");
 
-    std::string log_level = cfg_root["configuration"]["log"]["level"].asString();
-
-    std::cout << "Log level = " << log_level << std::endl;
-    
-    auto enemies = cfg_root["configuration"]["game"]["enemies"];
-    for (auto enemy: enemies)
-    {
-        std::cout << "Enemy " << enemy << std::endl;
-    }
-    
-    auto stages = cfg_root["configuration"]["game"]["stages"];
-    for (auto stage: stages)
-    {
-        std::cout << "Stage " << stage << std::endl;
-    }
     return EXIT_SUCCESS;
 } 
