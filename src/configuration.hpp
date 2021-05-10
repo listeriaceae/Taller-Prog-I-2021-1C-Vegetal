602bb371e6c62db24c0c2050296265c4f7b55f78
cdrubio <crubio@fi.uba.ar>
2021-05-10 08:28:49 -0700

    Update configuration.hpp

--- src/configuration.hpp
+++ src/configuration.hpp
@@ -1,9 +1,37 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace configuration
 {
+    class Enemy
+    {
+    public:
+        Enemy() {};
+        Enemy(const std::string& t, const unsigned int q) : type(t), quantity(q) {};
+        ~Enemy() {};
+
+        const std::string getType() const { return type; };
+        const unsigned int getQuantity() const { return quantity; };
+
+    private:
+        std::string type;
+        unsigned int quantity;
+    };
+    
+    class Stage
+    {
+    public:
+        Stage() {};
+        Stage(const std::vector<std::string> bgs) : backgrounds(bgs) {};
+
+        const std::vector<std::string> getBackgrounds() const { return backgrounds; };
+
+    private:
+        std::vector<std::string> backgrounds;
+    };
+
     class Configuration
     {
     public:
@@ -11,8 +39,12 @@ namespace configuration
         ~Configuration();
 
         const std::string getLogLevel() const { return log_level; };
+        const std::vector<configuration::Enemy> getEnemies() const { return enemies; };
+        const std::vector<configuration::Stage> getStages() const { return stages; };
 
     private:
         std::string log_level;
+        std::vector<Enemy> enemies;
+        std::vector<Stage> stages;
     };
 }
