c87407748b7670016ef87b7062dd70427c8acbb7
Aquista <kaquista@fi.uba.ar>
2021-06-12 15:12:40 -0300

    Agrego constantes a estadoNivel_t

--- src/utils/estadoNivel.h
+++ src/utils/estadoNivel.h
@@ -3,17 +3,16 @@
 #include "marioStructs.h"
 #include "punto.h"
 
+const int MAX_PLATFORMS = 12;
+const int MAX_ENEMIES = 3;
+const int MAX_BARRELS = 20;
+const int MAX_PLAYERS = 1;
+
 struct estadoNivel {
-        punto_t platforms[12];
-        punto_t enemies[3];
-        punto_t barrels[20];
-        estadoMario_t players[1];
-        /*
-        std::list<punto_t> platforms;
-        std::list<punto_t> enemies;
-        std::list<punto_t> barrels;
-        std::list<estadoMario_t> players;
-        */
+        punto_t platforms[MAX_PLATFORMS];
+        punto_t enemies[MAX_ENEMIES];
+        punto_t barrels[MAX_BARRELS];
+        estadoMario_t players[MAX_PLAYERS];
 };
 
 typedef struct estadoNivel estadoNivel_t;
