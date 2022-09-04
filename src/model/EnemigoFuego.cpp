ff13e64254ebf3da01a3beb912fe37637b0aab6c
Juan Ignacio Díaz <diazjignacio@gmail.com>
2022-09-04 12:22:49 -0300

    multiply after conversion

--- src/model/EnemigoFuego.cpp
+++ src/model/EnemigoFuego.cpp
@@ -6,7 +6,7 @@ EnemigoFuego::EnemigoFuego(punto32_t pos_,
                            int direccion,
                            fixed32_t min_,
                            fixed32_t max_)
-  : pos{ pos_ }, vel{ to_fixed32(direccion * ENEMIGO_FUEGO_VEL) },
+  : pos{ pos_ }, vel{ direccion * to_fixed32(ENEMIGO_FUEGO_VEL) },
     min{ min_ - to_fixed32(ANCHO_ENEMIGO_FUEGO) / 2 }, max{
       max_ - to_fixed32(ANCHO_ENEMIGO_FUEGO) / 2
     }
