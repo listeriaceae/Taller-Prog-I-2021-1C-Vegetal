247f3ab8a3608a2ba0ac7a22e18e5245b32a20ec
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-02-23 13:26:43 -0300

    formatting

--- src/controller/MarioController.cpp
+++ src/controller/MarioController.cpp
@@ -9,8 +9,10 @@ getControls()
   const auto *const keyboard = SDL_GetKeyboardState(NULL);
 
   AudioController::checkToggleMusicEvent(keyboard[SDL_SCANCODE_M]);
-  return keyboard[SDL_SCANCODE_SPACE] | keyboard[SDL_SCANCODE_UP] << 1
-         | keyboard[SDL_SCANCODE_DOWN] << 2 | keyboard[SDL_SCANCODE_LEFT] << 3
-         | keyboard[SDL_SCANCODE_RIGHT] << 4;
+  return keyboard[SDL_SCANCODE_SPACE] << 0
+       | keyboard[SDL_SCANCODE_UP]    << 1
+       | keyboard[SDL_SCANCODE_DOWN]  << 2
+       | keyboard[SDL_SCANCODE_LEFT]  << 3
+       | keyboard[SDL_SCANCODE_RIGHT] << 4;
 }
 }// namespace MarioController
